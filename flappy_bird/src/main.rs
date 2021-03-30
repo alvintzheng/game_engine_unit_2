9d05aab089cb0f5f574a5649d984347c5402c3d1
alvintzheng <alvintzheng@gmail.com>
2021-03-29 20:03:12 -0700

    Added menus

--- flappy_bird/src/main.rs
+++ flappy_bird/src/main.rs
@@ -55,6 +55,15 @@ const OBSTACLE_MIN_HEIGHT: u16 = 20;
 const OBSTACLE_SPEED: u16 = 2;
 const MIN_OBSTACLES: usize = WIDTH / (OBSTACLE_SPACING + OBSTACLE_WIDTH) as usize;
 
+#[derive(Debug, Copy, Clone)]
+enum Mode {
+    Title,
+    Play(bool),
+    Options,
+    ScoreBoard,
+    EndGame,
+}
+
 struct GameState {
     // What data do we need for this game?  Wall positions?
     // Colliders?  Sprites and stuff?
@@ -62,12 +71,111 @@ struct GameState {
     //textures: Vec<Rc<Texture>>,
     player: Entity,
     obstacles: Vec<Entity>,
+    accel_down: i32,
     //tiles: Vec<Tilemap>,
 }
 
 struct GameData {
-    //stuff like textures
+    obstacle_tex: Rc<Texture>,
+    title_tex: Rc<Texture>,
 }
+
+impl Mode {
+    // update consumes self and yields a new state (which might also just be self)
+    fn update(self, state: &mut GameState, data: &GameData, input: &WinitInputHelper) -> Self {
+        match self {
+            Mode::Title => {
+
+                if input.key_pressed(VirtualKeyCode::P) {
+                    Mode::Play(false)
+                }
+                else if input.key_pressed(VirtualKeyCode::O) {
+                    Mode::Options
+                } 
+                else if input.key_pressed(VirtualKeyCode::H) {
+                    Mode::ScoreBoard
+                }
+                else if input.key_pressed(VirtualKeyCode::Q) {
+                    panic!();
+                }
+                else {
+                    self
+                }
+            }
+            Mode::Play(paused) => {
+                if !paused {
+                    update_game(state, input, data);
+                }
+                if input.key_pressed(VirtualKeyCode::P) {
+                    Mode::Play(!paused)
+                }
+                else if input.key_pressed(VirtualKeyCode::T) {
+                    Mode::Title
+                }
+                else {
+                    self
+                }
+            }
+            Mode::Options => {
+                if input.key_pressed(VirtualKeyCode::T) {
+                    Mode::Title
+                }
+                else {
+                    self
+                }
+            }
+            Mode::ScoreBoard => {
+                if input.key_pressed(VirtualKeyCode::T) {
+                    Mode::Title
+                }
+                else {
+                    self
+                }
+            }
+            Mode::EndGame => {
+                if input.key_pressed(VirtualKeyCode::T) {
+                    Mode::Title
+                }
+                else {
+                    self
+                }
+            }
+        }
+    }
+    fn display(&self, state: &mut GameState, data: &GameData, screen: &mut Screen) {
+        match self {
+            Mode::Title => {
+                //draw a (static?) title
+                screen.clear(Rgba(80, 80, 80, 255));
+                let display_rect = Rect {
+                    x: 0,
+                    y: 0,
+                    w: 250,
+                    h: 51,
+                };
+                screen.bitblt(&data.title_tex, display_rect, Vec2i(0, 0));
+            }
+            Mode::Play(paused) => {
+                // Call screen's drawing methods to render the game state
+                screen.clear(Rgba(80, 80, 80, 255));
+
+                //draw each tilemap in vector to screen
+                draw_game(state, screen);
+            }
+            Mode::Options => {
+                screen.clear(Rgba(80, 255, 255, 255));
+            }
+            Mode::ScoreBoard => {
+                screen.clear(Rgba(255, 80, 255, 255));
+            }
+            Mode::EndGame => { // Draw game result?
+                screen.clear(Rgba(255, 255, 80, 255));
+            }
+        }
+    }
+}
+
+
 
 fn main() {
     let event_loop = EventLoop::new();
@@ -91,6 +199,8 @@ fn main() {
     //load assets
     let player_tex = Rc::new(Texture::with_file(Path::new("./res/sprites.png")));
     let obstacle_tex = Rc::new(Texture::with_file(Path::new("./res/Warp_pipe.png")));
+    let title_tex = Rc::new(Texture::with_file(Path::new("./res/TitleImage.png")));
+    
     let mut player_sprite = Sprite::new(
         &player_tex,
         Rect {
@@ -107,14 +217,19 @@ fn main() {
     let mut player = Entity::new(player_hitbox, player_sprite, true);
     
     let obstacles: Vec<Entity> = vec![];
-    
-    println!("{}", MIN_OBSTACLES);
+    let mut mode = Mode::Title;
 
     let mut state = GameState {
         // initial game state...
         player: player,
         obstacles: obstacles,
+        accel_down: 0,
         //tiles: tilemaps,
+    };
+
+    let mut data = GameData {
+        obstacle_tex: obstacle_tex,
+        title_tex: title_tex,
     };
 
     let mut camera_position = Vec2i(0,0);
@@ -130,27 +245,11 @@ fn main() {
     // Track end of the last frame
     let mut since = Instant::now();
     event_loop.run(move |event, _, control_flow| {
-
-        // let mut screen = Screen::wrap(pixels.get_frame(), WIDTH, HEIGHT, DEPTH, camera_position);
-        // screen.clear(CLEAR_COL);
-        // draw_game(&mut state, &mut screen);
-        // // Flip buffers
-        // if pixels.render().is_err() {
-        //     *control_flow = ControlFlow::Exit;
-        //     return;
-        // }
-
-        // delete old pipes and add new ones
-        // add new elems
-        
-
         // Draw the current frame
         if let Event::RedrawRequested(_) = event {
             let mut screen = Screen::wrap(pixels.get_frame(), WIDTH, HEIGHT, DEPTH, camera_position);
             screen.clear(CLEAR_COL);
-
-            println!("drawing");
-            draw_game(&mut state, &mut screen);
+            mode.display(&mut state, &mut data, &mut screen);
 
             // Flip buffers
             if pixels.render().is_err() {
@@ -163,7 +262,6 @@ fn main() {
             // The renderer "produces" time...
             let additional_time = since.elapsed().as_secs_f64();
             available_time += additional_time;
-            println!("{}", additional_time);
             // When did the last frame end?
             since = Instant::now();
         }
@@ -184,7 +282,7 @@ fn main() {
         while available_time >= DT {
             // Eat up one frame worth of time
             available_time -= DT;
-            update_game(&mut state, /*&input,*/ frame_count, &obstacle_tex);
+            mode = mode.update(&mut state, &data, &input);
 
             // Increment the frame counter
             frame_count += 1;
@@ -198,8 +296,6 @@ fn main() {
 fn draw_game(state: &mut GameState, screen: &mut Screen) {
     // Call screen's drawing methods to render the game state
     screen.clear(Rgba(80, 80, 80, 255));
-    println!("draw");
-
     for obs in state.obstacles.iter_mut() {
         screen.draw_entity(obs);
     }
@@ -207,31 +303,37 @@ fn draw_game(state: &mut GameState, screen: &mut Screen) {
 }
 
 
-fn update_game(state: &mut GameState, /*input: &WinitInputHelper,*/ frame: usize, obstacle_tex: &Rc<Texture>) {
-    // let player = &mut state.player.hitbox;
-    // // Determine player velocity
-    // let movespeed: i32 = 2;
-    // if input.key_held(VirtualKeyCode::Left) {
-    //     //player.vx = -1 * movespeed;
-    // } else if input.key_held(VirtualKeyCode::Right) {
-    //     //player.vx = 1 * movespeed;
-    // } else if input.key_pressed(VirtualKeyCode::Down) {
-    //     //player.vx = 0;
-    // } else {
-    //     player.vx = 0;
-    // }
-    // let mut accel_down = 1;
-    // if input.key_held(VirtualKeyCode::Up) {
-    //     accel_down -= 2;
-    // }
-    // player.vy += accel_down;
-    // //clamp velocity since this restitution assumes objects aren't speeding too much
-    // if player.vy > 4 {
-    //     player.vy = 4;
-    // }
-    // if player.vy < -4 {
-    //     player.vy = -4;
-    // }
+fn update_game(state: &mut GameState, input: &WinitInputHelper, data: &GameData) {
+    let player = &mut state.player.hitbox;
+    // Determine player velocity
+    let movespeed: i32 = 2;
+    if input.key_held(VirtualKeyCode::Left) {
+        //player.vx = -1 * movespeed;
+    } else if input.key_held(VirtualKeyCode::Right) {
+        //player.vx = 1 * movespeed;
+    } else if input.key_pressed(VirtualKeyCode::Down) {
+        //player.vx = 0;
+    } else {
+        player.vx = 0;
+    }
+    let mut accel_down = state.accel_down;
+    if input.key_pressed(VirtualKeyCode::Up) {
+        accel_down = -20;
+    } else {
+        accel_down += 1;
+        if accel_down > 1 {
+            accel_down = 1;
+        }
+    }
+    state.accel_down = accel_down;
+    player.vy += accel_down;
+    //clamp velocity since this restitution assumes objects aren't speeding too much
+    if player.vy > 4 {
+        player.vy = 4;
+    }
+    if player.vy < -4 {
+        player.vy = -4;
+    }
 
     if state.obstacles.len() < MIN_OBSTACLES {
         //println!("check");
@@ -249,7 +351,7 @@ fn update_game(state: &mut GameState, /*input: &WinitInputHelper,*/ frame: usize
                 vy: 0,
             };
             let mut new_sprite = Sprite::new(
-                &obstacle_tex,
+                &data.obstacle_tex,
                 Rect {
                     x: 0,
                     y: 0,
@@ -266,14 +368,11 @@ fn update_game(state: &mut GameState, /*input: &WinitInputHelper,*/ frame: usize
     }
 
     // check front pipe to see if it needs to be deleted
-
     if state.obstacles[0].hitbox.rect.x < 0 - OBSTACLE_WIDTH as i32 {
-            
             let _ = state.obstacles.remove(0);
-            println!("remove");
     }
+    state.player.hitbox.update();
 
-    println!("update");
     for mut obs in state.obstacles.iter_mut() {
         obs.hitbox.update();
     }
